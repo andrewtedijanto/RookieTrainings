8f3e3261c63670b13c0c54f0738e7d6f5e21035c
Andrew Tedijanto <firepanda73@gmail.com>
2015-11-09 16:21:34 -0800

    Here is the code. Hope it's good. e Please enter the commit message for your changes. Lines starting

--- src/MakeMath.java
+++ src/MakeMath.java
@@ -1,27 +1,41 @@
 
 public class MakeMath {
 	public static void main(String []args) {
-		// create an array with 4 indexes
+		int[] yourArray = new int[4]; 			// create an array with 4 indexes
 		
-		// create a for loop that while i is less than the 
-		// yourArray.length, then it will set the element to i at 
-		// the index of yourArray that is equal to i
-		// During this, print your array
-		
-		System.out.println("onto next");
-		
-		// for each in yourArray, add 1
-		// During this, print yourArray
+		for (int i = 0; i < yourArray.length; i++){// create a for loop that while i is less than the 
+			yourArray[i] = i;
+			System.out.println (yourArray[i]);	// yourArray.length, then it will set the element to i at 
+		}										// the index of yourArray that is equal to i
+												// During this, print your array
 		
 		System.out.println("onto next");
 		
-		// create an integer x with value 0
-		// while x is less than yourArray, add one to x
-		// during this, print x
+		for (int e = 0; e < yourArray.length; e++){						// for each in yourArray, add 1
+			yourArray[e] = e + 1;
+			System.out.println(yourArray[e]); // During this, print yourArray
+		}
+		
+		System.out.println("onto next");
+												// create an integer x with value 0
+		for (int x = 0; x < yourArray.length;) {					// while x is less than yourArray, add one to x
+			System.out.println(x); 				// during this, print x
+			x++;
+		}
 		
 		System.out.println("onto next");
 		
-		// for each element in yourArray
+		for (int each = -1; each < yourArray.length; each++) {
+			if (each < 0)
+				System.out.println("blah");
+			else if (each < 1)
+				System.out.println("bleh");
+			else if (each < 2)
+				System.out.println("blooh");
+			else
+				System.out.println("raaaaawr");
+		}
+	// for each element in yourArray
 		// if each is less than 0 print blah
 		// else if each is less than 1 print bleh
 		// else if each is less than 2 print blooh
